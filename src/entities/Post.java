81782e1e1d71a5e6369497c3ea7009f1fe40022e
Joao Lucas <jldecristo@gmail.com>
2025-07-26 16:32:20 -0300

    atributos da classe Post

--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,4 +1,16 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Post {
+
+    private LocalDate date;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    private final List<Comment> comments = new ArrayList<>();
+
 }
