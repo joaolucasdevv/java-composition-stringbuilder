ce039e71adaf865b5de83e7f7e53a841ef14f7f7
Joao Lucas <jldecristo@gmail.com>
2025-07-26 17:47:30 -0300

    Instanciando primeiro Post

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,25 @@
 package application;
 
 import entities.Comment;
+import entities.Post;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
         Comment c1 = new Comment("Have a nice trip!");
         Comment c2 = new Comment("Wow that's awesome!");
+
+        Post p1 = new Post(
+                LocalDateTime.parse("21/06/2018 13:05:44", fmt),
+                "Traveling to New Zealand",
+                "I'going to visit this wonderful contry!",
+                12);
 
     }
 
